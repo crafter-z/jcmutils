46361317c2e980d070587adfa796b523c298d047
crafter-z <xiaozq1245@163.com>
2023-03-16 09:48:17 +0800

    version1.1.1 修复柯勒照明光源生成问题

--- jcmutils/gen_sources.py
+++ jcmutils/gen_sources.py
@@ -5,35 +5,32 @@ def gen_kohler_sources(maxtheta, phi0, spacing, lambda0, flag_is_symmetry=False)
     maxtheta = np.deg2rad(maxtheta)
     phi0 = np.deg2rad(phi0)
     # 按spacing的间隔生成候选
-    candidate = np.linspace(-np.sin(maxtheta), np.sin(maxtheta), spacing)
+    candidate = np.linspace(-1, 1, spacing)
+    f=1/np.tan(maxtheta)
 
     # 提取在圆形孔径光阑内的平面上的等距点
     coordinate = []
-    for sx in candidate:
-        for sy in candidate:
-            if sx**2 + sy**2 <= np.sin(maxtheta)**2:
+    for mu in candidate:
+        for nu in candidate:
+            if mu**2 + nu**2 <= 1:
                 if flag_is_symmetry:
-                    if (sx < 0 and sy <=0) or (sx >=0 and sy < 0):
+                    if (mu < 0 and nu <=0) or (mu >=0 and nu < 0):
                         continue
-                coordinate.append([sx, sy])
+                coordinate.append([mu, nu])
 
     # 计算刚才得到的孔径光阑上的点对应的入射平面波
     keys = []
-    for i in coordinate:
+    for mu,nu in coordinate:
         phi = 0
-        theta = 0
-        if i[0] > 0 and i[1] >= 0:
-            phi = np.arctan(i[1]/i[0])
-            theta = np.arcsin(i[0]/np.cos(phi))
-        if i[0] <= 0 and i[1] > 0:
-            phi = np.pi/2 if i[0] == 0 else np.arctan(i[1]/i[0]) + np.pi
-            theta = np.arcsin(i[1]/np.sin(phi))
-        if i[0] < 0 and i[1] <= 0:
-            phi = np.arctan(i[1]/i[0]) + np.pi
-            theta = np.arcsin(i[0]/np.cos(phi))
-        if i[0] >= 0 and i[1] < 0:
-            phi = np.pi*3/2 if i[0] == 0 else np.arctan(i[1]/i[0]) + 2*np.pi
-            theta = np.arcsin(i[1]/np.sin(phi))
+        theta = np.abs(np.arctan(np.sqrt(mu**2+nu**2)/f))
+        if mu > 0 and nu >= 0:
+            phi = np.arctan(nu/mu)
+        if mu <=0 and nu > 0:
+            phi = np.pi/2 if mu == 0 else np.arctan(nu/mu) + np.pi
+        if mu < 0 and nu <=0:
+            phi = np.arctan(nu/mu) + np.pi
+        if mu >=0 and nu < 0:
+            phi = np.pi*3/2 if mu == 0 else np.arctan(nu/mu) + np.pi*2
         keys.append({'thetaphi': [theta, phi], 'lambda0': lambda0})
     # 通过透镜后的线偏振方向在入射平面中的投影直接就是p光，垂直于入射平面的就是s光
     # p方向的分量就是cos(PHI-psi0),s方向的分量就是sin(PHI-psi0),PHI是指入射光方位角。psi0是偏振方向与x轴夹角
