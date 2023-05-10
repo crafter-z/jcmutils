1232f9a28e444ab4355cd59d239dc1a5e721b9a9
crafter-z <xiaozq1245@163.com>
2023-05-10 11:06:38 +0800

    version1.4.0 1. 添加重要步骤计时功能 2. 优化代码质量

--- jcmutils/solver.py
+++ jcmutils/solver.py
@@ -4,6 +4,7 @@ from .logger import logger
 import os
 import shutil
 import cv2
+import datetime
 
 
 class solver:
@@ -23,13 +24,7 @@ class solver:
         logger.debug(f"solver parameters:jcmp_path-{jcmp_path};database_path-{abs_resultbag_dir}")
 
     def solve(self):
-        # 检查是否被以被初始化，若还未被初始化则报错
-        try:
-            if self.has_inited:
-                logger.debug("solver class have been inited")
-        except NameError:
-            print("Error ! please init solver befor using it!!!!!!")
-            raise Exception("Please init solver before using it")
+        self.__check_logger()
 
         # 初始化变量
         job_ids = []
@@ -40,13 +35,15 @@ class solver:
         while not no_error:
 
             # 开始计算
+            last_time = self.__start_timing()
             for key in waiting_keys:
                 job_id = jcmwave.solve(
                     self.jcmp_path, keys=key, temporary=True, resultbag=self.resultbag)
                 job_ids.append(job_id)
             logger.info("solve queue added done! start solving")
             jcmwave.daemon.wait(job_ids, resultbag=self.resultbag)
-            logger.info("solver program completed! analysing results...")
+            cost_time = self.__count_time(last_time)
+            logger.info(f"solver program completed!,cost{cost_time} analysing results...")
             no_error = True
 
             # 提取错误信息，如果是oom错误则加入队列重新计算
@@ -80,10 +77,8 @@ class solver:
         logger.info("analyse complete ! No error report ! solve mission done!!")
 
     def show_image(self, key, num_of_result, is_light_intense=False,vmax=None):
-        if not self.resultbag.check_result(key):
-            logger.error("get result failed! target key not find")
-            logger.error(f"the key is : {key}")
-            return -1
+        self.__check_logger()
+        self.check_result(key)
 
         # 开始提取
         result = self.resultbag.get_result(key)
@@ -97,13 +92,13 @@ class solver:
         cv2.imshow("image",field)
 
     def get_result(self, key):
+        self.__check_logger()
+        self.check_result(key)
         return self.resultbag.get_result(key)
 
     def save_image(self, target_directory, key, num_of_result, is_light_intense=False,vmax=None):
-        if not self.resultbag.check_result(key):
-            logger.error("get result failed! target key not find")
-            logger.error(f"the key is : {key}")
-            return -1
+        self.__check_logger()
+        self.check_result(key)
 
         # 开始提取
         result = self.resultbag.get_result(key)
@@ -112,19 +107,21 @@ class solver:
         if is_light_intense:
             field = np.power(field, 2)
         if not os.path.exists(target_directory):
-            logger.debug("target directory dosen't exist,creating...")
+            logger.info("target directory dosen't exist,creating...")
             os.makedirs(target_directory)
         vmaxa = np.max(field) if vmax is None else vmax
         field = (field / vmaxa)*235
         field = np.rot90(field)
         cv2.imwrite(target_directory.rstrip("/")+"output.jpg",field)
+        logger.info("target image saved successfully")
 
     def save_all_image(self, num_of_result, target_directory, is_light_intense=False, is_symmetry=False,vmax=None):
-        if not self.resultbag.check_result(self.keys[0]):
-            logger.error("get result failed! target key not find")
-            logger.error(f"the key is : {self.keys[0]}")
-            return -1
+        self.__check_logger()
+        self.check_result(self.keys[0])
 
+        # 计时
+        last_time = self.__start_timing()
+        
         # 开始提取
         # 先确定total_result的形状
         temp_result = self.resultbag.get_result(self.keys[0])
@@ -132,6 +129,7 @@ class solver:
                  temp_result[num_of_result]['field'][0]).sum(axis=2).real
         total_results = np.zeros(field.shape)
         logger.debug(f"total_result shape defined as {total_results.shape}")
+        
 
         # 开始逐个提取结果
         for key in self.keys:
@@ -164,8 +162,10 @@ class solver:
         sfield = (total_results/ vmaxa)*235
         file_name = target_directory.rstrip('/') + '/' + "total_result.jpg"
         cv2.imwrite(file_name,sfield)
-        logger.info("all target image saved completed!")
+        cost_time = self.__count_time(last_time)
+        logger.info(f"all target image saved completed! cost {cost_time}")
 
+    # 工具函数，处理词典，用处是将保存时的文件名缩短
     def __solve_dict(self, target_dict):
         res = ""
         for key, value in target_dict.items():
@@ -181,10 +181,34 @@ class solver:
         res.rstrip('-')
         return res
 
+    # 将root_dir目录下面的total_result.jpg移动至目标位置
     def move_total_results(self,root_dir,target_dir):
+        self.__check_logger()
         filelist = os.listdir(root_dir)
         for file in filelist:
             if file == "total_result.jpg":
                 if not os.path.exists(target_dir):
                     os.makedirs(target_dir)
                 shutil.copyfile(os.path.join(root_dir,file),os.path.join(target_dir,os.path.basename(root_dir) + ".jpg"))
+    
+    # 检查是否被以被初始化，若还未被初始化则报错
+    def __check_logger(self):
+        try:
+            if self.has_inited:
+                logger.debug("solver class have been inited")
+        except NameError:
+            print("Error ! please init solver befor using it!!!!!!")
+            raise Exception("Please init solver before using it")
+
+    # 检查是否存在目标键值，如无则报错
+    def check_result(self,key):
+        if not self.resultbag.check_result(key):
+            logger.error("get result failed! target key not find in resultbag")
+            logger.error(f"the key is : {key}")
+            raise Exception(f"get result failed! target key not find in resultbag,the key is {key}")
+    
+    def __start_timing(self):
+        return datetime.datetime.now()
+    
+    def __count_time(self,last_time):
+        return datetime.timedelta(datetime.datetime.now() - last_time)
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup,find_packages
 
-VERSION = '1.3.7'
+VERSION = '1.4.0'
 DESCRIPTION = "A general utils for jcmsuite"
 
 setup(
